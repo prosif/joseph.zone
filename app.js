82586f52b226c05f03906958c38e2bf74b12c54c
Joseph Garcia <prosif@gmail.com>
2020-04-12 17:30:09 -0700

    https + redirect

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const http = require('http');
 const fs = require('fs');
 const config = require('./config');
 
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
+
 const options = {
     key: fs.readFileSync(config.SSL_KEY_PATH),
     cert: fs.readFileSync(config.SSL_CERT_PATH)
 };
 
-const server = http.createServer(options, (req, res) => {
+const server = https.createServer(options, (req, res) => {
     const index = fs.readFileSync('index.html');
     res.setHeader('Content-Type', 'text/html');
     res.end(index);
 });
 
-server.listen(80);
+server.listen(HTTPS_PORT);
+
+http.createServer((req, res) => {
+    res.writeHead(301, {'Location': 'https://' + req.headers['host'] + req.url });
+    res.end();
+}).listen(HTTP_PORT);
 
 
