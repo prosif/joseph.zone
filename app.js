892bd9f6aec8d34dc7dc0f100c52770da901d517
Joseph Garcia <prosif@gmail.com>
2020-04-12 17:25:33 -0700

    add config for certs

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const fs = require('fs');
+const config = require('./config');
 
 const options = {
+    key: fs.readFileSync(config.SSL_KEY_PATH),
+    cert: fs.readFileSync(config.SSL_CERT_PATH)
 };
 
 const server = http.createServer(options, (req, res) => {
